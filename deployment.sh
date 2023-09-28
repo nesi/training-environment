2a872322c8ca2023114084d56be7a3fb0cb76bd1
Kahu Anderson <mrlbrick@gmail.com>
2023-09-29 10:16:59 +1300

    moved vars to same location

--- deployment.sh
+++ deployment.sh
@@ -5,9 +5,10 @@ case $1 in
     ansible-playbook setup-infra.yml -e operation=destroy -e terraform_workspace=${2:-default}
     # Need to also run this command 'kubectl delete cluster CLUSTER_NAME'
     # which ill probs base of the terraform workspace
+    kubectl delete cluster ${2:-default}
     ;;
 "create")
     ansible-playbook setup-infra.yml -e operation=create -e terraform_workspace=${2:-default}
-    ansible-playbook -i host.ini setup-training-environment.yml -u ubuntu --key-file ~/.ssh/id_flexi
+    ansible-playbook -i host.ini setup-training-environment.yml -u ubuntu --key-file ~/.ssh/id_flexi -e terraform_workspace=${2:-default}
     ;;
 esac
