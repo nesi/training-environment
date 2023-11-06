949cfa03cfd325b3c1984cc8ab6fd3fec4abe6d9
kahu.anderson@auckland.ac.nz <kahu.anderson@auckland.ac.nz>
2023-11-06 14:41:00 +1300

    major commit i probs broke something somewhere

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,11 +11,14 @@ required_version = ">= 0.14.0"
   backend "s3" {
     bucket = "training-environment"
     key    = "state/terraform.tfstate"
-    endpoint   = "https://object.akl-1.cloud.nesi.org.nz/"
-    sts_endpoint = "https://object.akl-1.cloud.nesi.org.nz/"
     region = "us-east-1"
-    force_path_style = "true"
-    skip_credentials_validation = "true"
+    use_path_style = "true"
+    skip_credentials_validation = "true"    
+    endpoints = {
+      s3 = "https://object.akl-1.cloud.nesi.org.nz/"
+    }
+    skip_requesting_account_id = "true"
+    skip_s3_checksum = "true"
   }
 }
 
