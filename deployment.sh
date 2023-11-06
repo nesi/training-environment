abbf3bf0064b273ca3265b1480c00be521781e63
kahu.anderson@auckland.ac.nz <kahu.anderson@auckland.ac.nz>
2023-11-07 12:35:41 +1300

    dont destroy again

--- deployment.sh
+++ deployment.sh
@@ -2,10 +2,10 @@
 
 case $1 in
 "destroy")
-    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i host.ini destroy-k8s-cluster.yml \
-        -u ${TF_VAR_vm_user} --key-file '${TF_VAR_key_file}' -e operation=destroy -e terraform_workspace=${2:-default} || true
-    ansible-playbook configure-route53.yml -e operation=destroy -e terraform_workspace=${2:-default}
-    ansible-playbook setup-infra.yml -e operation=destroy -e terraform_workspace=${2:-default}
+    # ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i host.ini destroy-k8s-cluster.yml \
+    #     -u ${TF_VAR_vm_user} --key-file '${TF_VAR_key_file}' -e operation=destroy -e terraform_workspace=${2:-default} || true
+    # ansible-playbook configure-route53.yml -e operation=destroy -e terraform_workspace=${2:-default}
+    # ansible-playbook setup-infra.yml -e operation=destroy -e terraform_workspace=${2:-default}
     ;;
 "create")
     ansible-playbook setup-infra.yml -e operation=create -e terraform_workspace=${2:-default}
